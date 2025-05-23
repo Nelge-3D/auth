68fc868f5d563e1d54029497159129c87651a7ac
Nelge-3D <louisgeriel.ngabi@gmail.com>
2025-05-23 12:50:44 +0300

    test1

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Tes règles globales ici (optionnel)
+  },
+  overrides: [
+    {
+      files: ['src/generated/prisma/**/*.js', 'src/generated/prisma/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ],
+};
